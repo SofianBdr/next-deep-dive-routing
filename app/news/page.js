b5392c9148ff6da083c7b66ba708f4c0088054b4
SofianBdr <sofianboudraa.msr@gmail.com>
2026-01-17 10:45:07 +0100

    Archive

---
+++ app/news/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import {DUMMY_NEWS} from "@/dummy-news";
+import Image from "next/image";
+import NewsList from "@/components/news-list";
+
+export default function NewsPage() {
+    return (
+        <>
+            <h1>News page</h1>
+            <NewsList news={DUMMY_NEWS} />
+        </>
+    )
+}
